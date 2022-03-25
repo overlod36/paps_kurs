81dedbf756a10b62efdcd5b1a2cd23e0cede71b1
kirbi <Biehan@mail.ru>
2022-03-25 19:40:12 +0300

    writing client code

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from pyautogui import *
-from datatime import *
-screenshot(r"e:/projects/paps_kurs/scr.png")
+import socket
+import sys
+
+HOST, PORT = "localhost", 8080
+data = " ".join(sys.argv[1:])
+
+try:
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock: #SOCK_STREAM - TCP сокет
+		sock.connect((HOST, PORT))
+		sock.sendall(bytes(data + "\n", "utf-8"))
+		received = str(sock.recv(1024), "utf-8")
+		print("Отправленная информация -> ", data)
+	print("Полученная информация -> ", received)
+except ConnectionRefusedError:
+	print("Соединение не было установлено!")
 
 
